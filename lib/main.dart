743671110b3714ccff048b381a0ec9f5a7e2372a
Cho Su Wai <chosuwai@gmail.com>
2024-06-30 21:49:45 +0700

    Project Skeleton

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+    );
+  }
+}
+
